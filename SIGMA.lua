65d8e4539f4e1fe3f3e8f48587f9b7b8858f6359
TimShadow795 <162728677+TimShadow795@users.noreply.github.com>
2024-10-04 22:21:04 +0300

    Update and rename .gitignore to SIGMA.lua

---
+++ SIGMA.lua
@@ -0,0 +1,541 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local g00byd0lan = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+local Parcticles = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local Sky = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Disco = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local Spam = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local Music = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local UICorner_6 = Instance.new("UICorner")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local UICorner_7 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_8 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local UICorner_9 = Instance.new("UICorner")
+local Page2 = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+local TextLabel_3 = Instance.new("TextLabel")
+local UICorner_11 = Instance.new("UICorner")
+local Page2_2 = Instance.new("ImageLabel")
+local UICorner_12 = Instance.new("UICorner")
+local ImageLabel_3 = Instance.new("ImageLabel")
+local UICorner_13 = Instance.new("UICorner")
+local TextLabel_4 = Instance.new("TextLabel")
+local UICorner_14 = Instance.new("UICorner")
+local TextLabel_5 = Instance.new("TextLabel")
+local UICorner_15 = Instance.new("UICorner")
+local TextLabel_6 = Instance.new("TextLabel")
+local UICorner_16 = Instance.new("UICorner")
+local Page1 = Instance.new("TextButton")
+local UICorner_17 = Instance.new("UICorner")
+local Kill = Instance.new("TextButton")
+local UICorner_18 = Instance.new("UICorner")
+
+--Properties:
+
+g00byd0lan.Name = "g00byd0lan"
+g00byd0lan.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+g00byd0lan.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageLabel.Parent = g00byd0lan
+ImageLabel.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.0464046821, 0, 0.111027189, 0)
+ImageLabel.Size = UDim2.new(0, 230, 0, 377)
+ImageLabel.Image = "rbxassetid://12285082171"
+
+Parcticles.Name = "Parcticles"
+Parcticles.Parent = ImageLabel
+Parcticles.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Parcticles.BackgroundTransparency = 0.400
+Parcticles.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Parcticles.BorderSizePixel = 0
+Parcticles.Position = UDim2.new(0.0652173907, 0, 0.278397143, 0)
+Parcticles.Size = UDim2.new(0, 200, 0, 44)
+Parcticles.Font = Enum.Font.AmaticSC
+Parcticles.Text = "Particles"
+Parcticles.TextColor3 = Color3.fromRGB(255, 255, 255)
+Parcticles.TextScaled = true
+Parcticles.TextSize = 14.000
+Parcticles.TextWrapped = true
+
+UICorner.Parent = Parcticles
+
+Sky.Name = "Sky"
+Sky.Parent = ImageLabel
+Sky.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Sky.BackgroundTransparency = 0.400
+Sky.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Sky.BorderSizePixel = 0
+Sky.Position = UDim2.new(0.0652173907, 0, 0.736534238, 0)
+Sky.Size = UDim2.new(0, 200, 0, 44)
+Sky.Font = Enum.Font.AmaticSC
+Sky.Text = "Sky"
+Sky.TextColor3 = Color3.fromRGB(255, 255, 255)
+Sky.TextScaled = true
+Sky.TextSize = 14.000
+Sky.TextWrapped = true
+
+UICorner_2.Parent = Sky
+
+Disco.Name = "Disco"
+Disco.Parent = ImageLabel
+Disco.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Disco.BackgroundTransparency = 0.400
+Disco.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Disco.BorderSizePixel = 0
+Disco.Position = UDim2.new(0.0652173907, 0, 0.589421093, 0)
+Disco.Size = UDim2.new(0, 200, 0, 44)
+Disco.Font = Enum.Font.AmaticSC
+Disco.Text = "Disco"
+Disco.TextColor3 = Color3.fromRGB(255, 255, 255)
+Disco.TextScaled = true
+Disco.TextSize = 14.000
+Disco.TextWrapped = true
+
+UICorner_3.Parent = Disco
+
+Spam.Name = "Spam"
+Spam.Parent = ImageLabel
+Spam.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Spam.BackgroundTransparency = 0.400
+Spam.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Spam.BorderSizePixel = 0
+Spam.Position = UDim2.new(0.0652173907, 0, 0.431984365, 0)
+Spam.Size = UDim2.new(0, 200, 0, 44)
+Spam.Font = Enum.Font.AmaticSC
+Spam.Text = "Decal Spam"
+Spam.TextColor3 = Color3.fromRGB(255, 255, 255)
+Spam.TextScaled = true
+Spam.TextSize = 14.000
+Spam.TextWrapped = true
+
+UICorner_4.Parent = Spam
+
+Music.Name = "Music"
+Music.Parent = ImageLabel
+Music.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Music.BackgroundTransparency = 0.400
+Music.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Music.BorderSizePixel = 0
+Music.Position = UDim2.new(0.0652173907, 0, 0.121791482, 0)
+Music.Size = UDim2.new(0, 200, 0, 44)
+Music.Font = Enum.Font.AmaticSC
+Music.Text = "Music"
+Music.TextColor3 = Color3.fromRGB(255, 255, 255)
+Music.TextScaled = true
+Music.TextSize = 14.000
+Music.TextWrapped = true
+
+UICorner_5.Parent = Music
+
+UICorner_6.Parent = ImageLabel
+
+ImageLabel_2.Parent = ImageLabel
+ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_2.BackgroundTransparency = 0.500
+ImageLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_2.BorderSizePixel = 0
+ImageLabel_2.Position = UDim2.new(0.0652173907, 0, 0.01591512, 0)
+ImageLabel_2.Size = UDim2.new(0, 31, 0, 31)
+ImageLabel_2.Image = "rbxassetid://12285082171"
+
+UICorner_7.Parent = ImageLabel_2
+
+TextLabel.Parent = ImageLabel_2
+TextLabel.BackgroundColor3 = Color3.fromRGB(167, 167, 167)
+TextLabel.BackgroundTransparency = 0.500
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(1.23684216, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 153, 0, 34)
+TextLabel.Font = Enum.Font.AmaticSC
+TextLabel.Text = "G00byd0lan Gui"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+UICorner_8.Parent = TextLabel
+
+TextLabel_2.Parent = ImageLabel_2
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(167, 167, 167)
+TextLabel_2.BackgroundTransparency = 0.500
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(1, 0, 10.6129036, 0)
+TextLabel_2.Size = UDim2.new(0, 114, 0, 26)
+TextLabel_2.Font = Enum.Font.AmaticSC
+TextLabel_2.Text = "Page 1"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+UICorner_9.Parent = TextLabel_2
+
+Page2.Name = "Page2"
+Page2.Parent = ImageLabel
+Page2.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Page2.BackgroundTransparency = 0.400
+Page2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Page2.BorderSizePixel = 0
+Page2.Position = UDim2.new(0.734782636, 0, 0.877759695, 0)
+Page2.Size = UDim2.new(0, 35, 0, 36)
+Page2.Font = Enum.Font.AmaticSC
+Page2.Text = ">"
+Page2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Page2.TextScaled = true
+Page2.TextSize = 14.000
+Page2.TextWrapped = true
+
+UICorner_10.Parent = Page2
+
+TextLabel_3.Parent = ImageLabel
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(167, 167, 167)
+TextLabel_3.BackgroundTransparency = 0.500
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.200000003, 0, -0.0822281167, 0)
+TextLabel_3.Size = UDim2.new(0, 133, 0, 31)
+TextLabel_3.Font = Enum.Font.AmaticSC
+TextLabel_3.Text = "Standart Functions"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
+
+UICorner_11.Parent = TextLabel_3
+
+Page2_2.Name = "Page2"
+Page2_2.Parent = g00byd0lan
+Page2_2.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
+Page2_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Page2_2.BorderSizePixel = 0
+Page2_2.Position = UDim2.new(0.0464046821, 0, 0.111027189, 0)
+Page2_2.Size = UDim2.new(0, 230, 0, 377)
+Page2_2.Visible = false
+Page2_2.Image = "rbxassetid://12285082171"
+
+UICorner_12.Parent = Page2_2
+
+ImageLabel_3.Parent = Page2_2
+ImageLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_3.BackgroundTransparency = 0.500
+ImageLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_3.BorderSizePixel = 0
+ImageLabel_3.Position = UDim2.new(0.0652173907, 0, 0.01591512, 0)
+ImageLabel_3.Size = UDim2.new(0, 31, 0, 31)
+ImageLabel_3.Image = "rbxassetid://12285082171"
+
+UICorner_13.Parent = ImageLabel_3
+
+TextLabel_4.Parent = ImageLabel_3
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(167, 167, 167)
+TextLabel_4.BackgroundTransparency = 0.500
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(1.23684216, 0, 0, 0)
+TextLabel_4.Size = UDim2.new(0, 153, 0, 34)
+TextLabel_4.Font = Enum.Font.AmaticSC
+TextLabel_4.Text = "G00byd0lan Gui"
+TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 14.000
+TextLabel_4.TextWrapped = true
+
+UICorner_14.Parent = TextLabel_4
+
+TextLabel_5.Parent = Page2_2
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(167, 167, 167)
+TextLabel_5.BackgroundTransparency = 0.500
+TextLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.230434775, 0, 0.891246676, 0)
+TextLabel_5.Size = UDim2.new(0, 114, 0, 26)
+TextLabel_5.Font = Enum.Font.AmaticSC
+TextLabel_5.Text = "Page 2"
+TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.TextScaled = true
+TextLabel_5.TextSize = 14.000
+TextLabel_5.TextWrapped = true
+
+UICorner_15.Parent = TextLabel_5
+
+TextLabel_6.Parent = Page2_2
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(167, 167, 167)
+TextLabel_6.BackgroundTransparency = 0.500
+TextLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.200000003, 0, -0.0822281167, 0)
+TextLabel_6.Size = UDim2.new(0, 133, 0, 31)
+TextLabel_6.Font = Enum.Font.AmaticSC
+TextLabel_6.Text = "Server Destruction"
+TextLabel_6.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 14.000
+TextLabel_6.TextWrapped = true
+
+UICorner_16.Parent = TextLabel_6
+
+Page1.Name = "Page1"
+Page1.Parent = Page2_2
+Page1.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Page1.BackgroundTransparency = 0.400
+Page1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Page1.BorderSizePixel = 0
+Page1.Position = UDim2.new(0.0478260852, 0, 0.875107169, 0)
+Page1.Size = UDim2.new(0, 35, 0, 36)
+Page1.Font = Enum.Font.AmaticSC
+Page1.Text = "<"
+Page1.TextColor3 = Color3.fromRGB(255, 255, 255)
+Page1.TextScaled = true
+Page1.TextSize = 14.000
+Page1.TextWrapped = true
+
+UICorner_17.Parent = Page1
+
+Kill.Name = "Kill"
+Kill.Parent = Page2_2
+Kill.BackgroundColor3 = Color3.fromRGB(111, 111, 111)
+Kill.BackgroundTransparency = 0.400
+Kill.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Kill.BorderSizePixel = 0
+Kill.Position = UDim2.new(0.0652173907, 0, 0.119925015, 0)
+Kill.Size = UDim2.new(0, 200, 0, 44)
+Kill.Font = Enum.Font.AmaticSC
+Kill.Text = "Shutdown"
+Kill.TextColor3 = Color3.fromRGB(255, 255, 255)
+Kill.TextScaled = true
+Kill.TextSize = 14.000
+Kill.TextWrapped = true
+
+UICorner_18.Parent = Kill
+
+-- Scripts:
+
+local function TVCGYY_fake_script() -- ImageLabel.Draggable 
+	local script = Instance.new('LocalScript', ImageLabel)
+
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end
+coroutine.wrap(TVCGYY_fake_script)()
+local function GORF_fake_script() -- ImageLabel.ButtonsWork 
+	local script = Instance.new('LocalScript', ImageLabel)
+
+	local frame = script.Parent
+	
+	frame.Page2.MouseButton1Click:Connect(function()
+		game.StarterGui.g00byd0lan.Page2.Visible = true
+		task.wait(0.1)
+		game.StarterGui.g00byd0lan.ImageLabel.Visible = false
+	end)
+	
+	frame.Music.MouseButton1Click:Connect(function()
+		local music = Instance.new("Sound")
+		music.Parent = game.SoundService
+		music.Name = "Music"
+		music.SoundId = "rbxassetid://1847661821"	
+		music.Looped = true
+		music.Playing = true
+	end)
+	
+	frame.Parcticles.MouseButton1Click:Connect(function()
+		t3 = "http://www.roblox.com/asset/?id=1569584752"
+		local p = game.Players:GetChildren()
+		local w = game.Workspace:GetChildren()
+		for i,v in pairs(p) do
+			local pe = Instance.new("ParticleEmitter", v.Character.Head)
+			pe.Texture = t3
+			pe.VelocitySpread = 100
+		end
+		for _, Object in pairs(game.Workspace:GetChildren()) do
+			if Object.ClassName == "Part" or "Model" then
+				local fe = Instance.new("ParticleEmitter", Object)
+				fe.Texture = t3
+			end
+		end
+		
+		t3 = "http://www.roblox.com/asset/?id=12811030246"
+		local p = game.Players:GetChildren()
+		local w = game.Workspace:GetChildren()
+		for i,v in pairs(p) do
+			local pe = Instance.new("ParticleEmitter", v.Character.Head)
+			pe.Texture = t3
+			pe.VelocitySpread = 100
+		end
+		for _, Object in pairs(game.Workspace:GetChildren()) do
+			if Object.ClassName == "Part" or "Model" then
+				local fe = Instance.new("ParticleEmitter", Object)
+				fe.Texture = t3
+			end
+		end
+		
+		t3 = "http://www.roblox.com/asset/?id=626769954"
+		local p = game.Players:GetChildren()
+		local w = game.Workspace:GetChildren()
+		for i,v in pairs(p) do
+			local pe = Instance.new("ParticleEmitter", v.Character.Head)
+			pe.Texture = t3
+			pe.VelocitySpread = 100
+		end
+		for _, Object in pairs(game.Workspace:GetChildren()) do
+			if Object.ClassName == "Part" or "Model" then
+				local fe = Instance.new("ParticleEmitter", Object)
+				fe.Texture = t3
+			end
+		end
+	end)
+	
+	frame.Spam.MouseButton1Click:Connect(function()
+		local _Workspace = game:GetService("Workspace")
+		local Surfaces = {"Front", "Back", "Left", "Right", "Top", "Bottom"}
+		local ID = "http://www.roblox.com/asset/?id=12285157525"
+		for _, Part in pairs(_Workspace:GetDescendants()) do
+			if Part:IsA("BasePart") and Part.Name ~= "Camera" and Part.Name ~= "Terrain" then
+				for i = 1, #Surfaces do
+					local Decal = Instance.new("Decal")
+					Decal.Texture = ID
+					Decal.ZIndex = 10
+					Decal.Name = "Decal"..Surfaces[i]
+					Decal.Parent = Part
+					Decal.Face = Surfaces[i]
+				end
+			end
+		end
+	end)
+	
+	frame.Sky.MouseButton1Click:Connect(function()
+		local ID = "http://www.roblox.com/asset/?id=12285157525"
+		local Sky = Instance.new("Sky")
+		Sky.Parent = game.Lighting
+		Sky.Name = "NewSky"
+		Sky.MoonTextureId = ID
+		Sky.SkyboxBk = ID
+		Sky.SkyboxDn = ID
+		Sky.SkyboxFt = ID
+		Sky.SkyboxLf = ID
+		Sky.SkyboxRt = ID
+		Sky.SkyboxUp = ID
+		Sky.SunTextureId = ID
+	end)
+	
+	frame.Disco.MouseButton1Click:Connect(function()
+		while true do
+			wait(.5)
+			game.Lighting.Ambient = Color3.new(math.random(), math.random(), math.random())
+		end
+	end)
+end
+coroutine.wrap(GORF_fake_script)()
+local function CFVHOT_fake_script() -- ImageLabel.DiscoButtonRgb 
+	local script = Instance.new('LocalScript', ImageLabel)
+
+	local frame = script.Parent
+	
+	while true do
+		frame.Disco.BackgroundColor3 = Color3.new(math.random(), math.random(), math.random())
+		task.wait(0.1)
+	end
+end
+coroutine.wrap(CFVHOT_fake_script)()
+local function JZHOMJR_fake_script() -- Page2_2.Draggable 
+	local script = Instance.new('LocalScript', Page2_2)
+
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end
+coroutine.wrap(JZHOMJR_fake_script)()
+local function NDFMA_fake_script() -- Page2_2.ButtonsWork 
+	local script = Instance.new('LocalScript', Page2_2)
+
+	local frame = script.Parent
+	
+	frame.Page1.MouseButton1Click:Connect(function()
+		game.StarterGui.g00byd0lan.Page2.Visible = false
+		task.wait(0.1)
+		game.StarterGui.g00byd0lan.ImageLabel.Visible = true
+	end)
+	
+	frame.Kill.MouseButton1Click:Connect(function()
+		local plr = game.Players.LocalPlayer
+		plr.Kick("HACKED BY G00BYD0LAN!")
+	end)
+end
+coroutine.wrap(NDFMA_fake_script)()
